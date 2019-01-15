6e366d2646cbe6b411f5052476374eab62e830df
Samidh Patel <spatel13456@gmail.com>
2019-01-15 04:11:08 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,10 @@ def second_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }}
 
   
